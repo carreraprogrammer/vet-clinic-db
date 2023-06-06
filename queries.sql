6de34e3f7a9961eb4617b1e67198da5fce72d40b
Daniel <daniel.carrera@dataico.com>
2023-06-06 17:32:31 -0500

    Query for name of all animals that are neutered and have less than 3 escape attempts.

--- queries.sql
+++ queries.sql
@@ -3,5 +3,8 @@
 SELECT * FROM animals
 WHERE name LIKE '%mon';
 
-SELECT * FROM animals
+SELECT name FROM animals
 WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+
+SELECT name FROM animals
+WHERE neutered = TRUE AND escape_attempts < 3;
