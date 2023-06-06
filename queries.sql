0d818e03ce24fec73013faa02efd7837707b107c
Daniel <daniel.carrera@dataico.com>
2023-06-06 17:37:31 -0500

    query for he date of birth of all animals named either "Agumon" or "Pikachu".

--- queries.sql
+++ queries.sql
@@ -8,3 +8,6 @@ WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 
 SELECT name FROM animals
 WHERE neutered = TRUE AND escape_attempts < 3;
+
+SELECT date_of_birth FROM animals
+WHERE name LIKE 'Agumon' OR name LIKE 'Pikachu';
