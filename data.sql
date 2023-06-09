ffadb9ca1a7fd4cd518cdd253b3d3945ebc09547
Daniel <daniel.carrera@dataico.com>
2023-06-09 17:23:37 -0500

    Add information into specializations table

--- data.sql
+++ data.sql
@@ -54,3 +54,10 @@ VALUES
     ('Maisy Smith', 26, '2019-01-17'),
     ('Stephanie Mendez', 64, '1981-05-04'),
     ('Jack Harkness', 38, '2008-06-08');
+
+INSERT INTO specializations(vet_id, species_id)
+VALUES 
+        (1, 1),
+        (3, 1),
+        (3, 2),
+        (4, 2);
