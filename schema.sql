824afb8b6dba3f3ab05ccc43ce334ff3aebd4149
Daniel <carreraprogrammer@gmail.com>
2023-06-12 15:26:41 -0500

    Add queries to analyze

--- schema.sql
+++ schema.sql
@@ -66,4 +66,8 @@ CREATE TABLE visits(
   date_of_visits DATE
 );
 
+SELECT COUNT(*) FROM visits where animal_id = 4;
 
+SELECT * FROM visits where vet_id = 2;
+
+SELECT * FROM owners where email = 'owner_18327@mail.com';
