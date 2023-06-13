c80daa69a80e90fb5f7dd272e8f8df4ffc26e433
Daniel <carreraprogrammer@gmail.com>
2023-06-13 09:34:38 -0500

    Add queries to analyze and improve

--- queries.sql
+++ queries.sql
@@ -230,3 +230,9 @@ JOIN animals a
 ON a.id = vi.animal_id
 WHERE vt.name = 'Maisy Smith'
 GROUP BY vt.name, a.species;
+
+SELECT COUNT(*) FROM visits where animal_id = 4;
+
+SELECT * FROM visits where vet_id = 2;
+
+SELECT * FROM owners where email = 'owner_18327@mail.com';
